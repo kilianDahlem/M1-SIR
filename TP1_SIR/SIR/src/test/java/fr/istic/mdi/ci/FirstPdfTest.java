8efa4ad9c6e6981a9419ec6237fbee59d216ef78
kilianDahlem <dahlem.kilian@gmail.com>
2023-01-31 09:23:35 +0100

    Project INIT
    Add TP1

---
+++ TP1_SIR/SIR/src/test/java/fr/istic/mdi/ci/FirstPdfTest.java
@@ -0,0 +1,21 @@
+package fr.istic.mdi.ci;
+
+import com.itextpdf.text.DocumentException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstPdfTest {
+    //FirstPdf firstPdf;
+    @BeforeEach
+    public void init(){
+        FirstPdf firstPdf = new FirstPdf();
+    }
+
+    @Test
+public void TestCreateList() throws DocumentException {
+        assertTrue(true);
+    }
+}
